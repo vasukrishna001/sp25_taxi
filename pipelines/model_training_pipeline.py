0f410811063e6b9c2958e42876d5f7feeba80db5
vasukrishna001 <vasubandike001@gmail.com>
2025-03-01 14:45:53 -0500

    Model_pipeline updated

--- pipelines/model_training_pipeline.py
+++ pipelines/model_training_pipeline.py
@@ -51,6 +51,6 @@ if test_mae < metric.get("test_mae"):
         input_example=features.sample(),
         model_schema=model_schema,
     )
-    model.save(model_path)
+    model.save(str(model_path))
 else:
     print(f"Skipping model registration because new model is not better!")
